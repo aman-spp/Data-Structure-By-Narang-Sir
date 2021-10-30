66f7234cfd685edecba2caec907fdb0be6dad6f5
Aman Babu <72257053+aman-spp@users.noreply.github.com>
2021-10-31 03:03:47 +0530

    Data Structure by Narang sir

---
+++ Array_passing.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int a[], int n)
+{
+    // int n = sizeof(a)/sizeof(int);   ///////////  [a] ---> {1,2,3,4,5,6,7,8,9} so size of a equal size of pointer variable 8 or 4;
+    // so n = either 1 or 2.  so passing n is necessary traversing array.
+    for(int i=0;i<n;i++)
+    {
+        cout<<"["<<a[i]<<"]"<<" ";
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr)/sizeof(int);
+    printarray(arr,n);
+    return 0;
+}
