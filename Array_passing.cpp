c01e0ef95915fc6a0ad912814d8aae933e71c35e
Aman Babu <72257053+aman-spp@users.noreply.github.com>
2021-10-31 03:07:56 +0530

    3

--- Array_passing.cpp
+++ Array_passing.cpp
@@ -5,6 +5,7 @@ void printarray(int a[], int n)
 {
     // int n = sizeof(a)/sizeof(int);   ///////////  [a] ---> {1,2,3,4,5,6,7,8,9} so size of a equal size of pointer variable 8 or 4;
     // so n = either 1 or 2.  so passing n is necessary for traversing array.
+    // And array always pass by reference not by value
     for(int i=0;i<n;i++)
     {
         cout<<"["<<a[i]<<"]"<<" ";
